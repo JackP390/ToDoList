64501b0ca6513d969c8882a3f1b51425f3fa83e8
JackP390 <patatamati123@gmail.com>
2025-11-06 21:16:00 -0300

    Cosas importantes (las bases)

--- ToDoApp/models.py
+++ ToDoApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=500)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    estado = models.BooleanField(default=False)
--- ToDoApp/views.py
+++ ToDoApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from.forms import FormTarea
+from .models import Tarea
 
 # Create your views here.
+def main(request):
+    if request.method == 'POST':
+        form = FormTarea(request.POST)
+        if form.is_valid():
+            form.save()
+    form = FormTarea()
+    tarea = Tarea.objects.all()
+    context = {'form':form, 'tarea':tarea}
+    return render(request, 'main.html', context)
